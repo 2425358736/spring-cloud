3d8e01b4498600ceb4277b4ea6a18631285f0c20
2425358736 <2425358736@qq.com>
2019-04-16 15:09:40 +0800

    oath2.0使用

--- auth2-server/src/main/java/com/springcloud/demo/config/WebSecurityConfig.java
+++ auth2-server/src/main/java/com/springcloud/demo/config/WebSecurityConfig.java
@@ -22,7 +22,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
